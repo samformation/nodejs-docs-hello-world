765bd9df50889162e82dff56995e53caea51b5f3
samformation <78386381+samformation@users.noreply.github.com>
2023-08-31 14:12:59 +0200

    Update index.js
    test d'execution dela tache jenkins

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 // https://github.com/microsoft/Web-Dev-For-Beginners/tree/main/7-bank-project/api
 // ***************************************************************************
 
+response.end("Hello world!");
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors')
