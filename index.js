04f28c73fca9824275b5e09a57f607f90f3e507f
samformation <78386381+samformation@users.noreply.github.com>
2023-08-31 14:44:38 +0200

    Update index.js
    remise en place du code

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 // Bank API code from Web Dev For Beginners project
 // https://github.com/microsoft/Web-Dev-For-Beginners/tree/main/7-bank-project/api
 // ***************************************************************************
-
-response.end("Hello world!");
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors')
